d280ebb4242dbd9ef0612cad365c58bbcc6e0c6d
Sebastian Kranz <sebkranz@gmail.com>
2023-12-21 08:46:12 +0100

    Update install.R

--- scripts/install.R
+++ scripts/install.R
@@ -3,3 +3,4 @@
 library(remotes)
 remotes::install_github("repboxr/sourcemodify", upgrade="never", force=FALSE)
 remotes::install_github("repboxr/repboxMap", upgrade="never", force=FALSE)
+remotes::install_github("repboxr/repboxHtml", upgrade="never", force=FALSE)
